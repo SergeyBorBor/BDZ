ab52f9f8a78648c94486b76b49e0642ed93c0f59
SergeyBorBor <153349346+SergeyBorBor@users.noreply.github.com>
2023-12-23 15:07:10 +0300

    Update brun.cpp

--- obj/brun.cpp
+++ obj/brun.cpp
@@ -1,10 +1,10 @@
 #include <cstdlib>
 int main() {
     // Запуск программы b1.cpp
-    system("g++ ../src/b1.cpp -o p1 && ./p1");
+    system("g++ ../src/b1.cpp -o b1 && ./b1");
 
     // Запуск программы b2.cpp
-    system("g++ ../src/b2.cpp -o p2 && ./p2");
+    system("g++ ../src/b2.cpp -o b2 && ./b2");
 
     return 0;
 }
