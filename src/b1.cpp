1193600a6350fee1b1b58db4385c30fadcbb67b2
SergeyBorBor <153349346+SergeyBorBor@users.noreply.github.com>
2024-01-04 18:59:16 +0300

    Update b1.cpp

--- src/b1.cpp
+++ src/b1.cpp
@@ -121,8 +121,8 @@ int main() {
     }
     else
     { 
-       // sleep(10);
-       printf(" Результаты мониторинга системы успешно записаны в файл log.txt.\n");
+       sleep(10);
+       printf(" Результаты мониторинга записаны в файл log.txt.\n");
        // Открытие файла log.txt для записи
        int fd = open("../filles/log.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
        if (fd == -1) 
@@ -132,38 +132,34 @@ int main() {
        }
 
        // Дублирование дескриптора файлового дескриптора stdout для записи в файл
-       if (dup2(fd, STDOUT_FILENO) == -1) 
-       {
-         perror("dup2");
-         exit(1);
-       }
-
+       dup2(fd, STDOUT_FILENO); 
+       
        // Отображение информации о системе
        printf("System Monitoring");
 
       // Код мониторинга системы
       // Выведем информацию о текущем процессе и системном времени
-      int pid = getpid();
-      printf("Current process ID: %d", pid);
-      time_t current_time = time();
+      int pid1 = getpid();
+      printf("Current process ID: %d", pid1);
+      time_t current_time = time(NULL);
       printf("Current time: %s", ctime(&current_time));
 
       // Получение информации об использовании оперативной памяти
-      struct sysinfo mem_info;
-      sysinfo(&mem_info);
-      printf("Total RAM: %ld MB", mem_info.totalram / 1024 / 1024);
-      printf("Free RAM: %ld MB", mem_info.freeram / 1024 / 1024);
+      struct sysinfo mem;
+      sysinfo(&mem);
+      printf("Total RAM: %ld MB", mem.totalram / 1024 / 1024);
+      printf("Free RAM: %ld MB", mem.freeram / 1024 / 1024);
 
       // Получение информации об использовании процессора
-      struct rusage cpu_info;
-      getrusage(RUSAGE_SELF, &cpu_info);
-      printf("CPU Usage: %ld.%06ld seconds", cpu_info.ru_utime.tv_sec, cpu_info.ru_utime.tv_usec);
+      struct rusage cpu;
+      getrusage(RUSAGE_SELF, &cpu);
+      printf("CPU Usage: %ld.%06ld seconds", cpu.ru_utime.tv_sec, cpu.ru_utime.tv_usec);
 
       // Получение информации о дисковом пространстве
-      struct statvfs disk_info;
-      statvfs(".", &disk_info);
-      printf("Total Disk Space: %ld GB", (disk_info.f_blocks * disk_info.f_frsize) / 1024 / 1024 / 1024);
-      printf("Available Disk Space: %ld GB", (disk_info.f_bavail * disk_info.f_frsize) / 1024 / 1024 / 1024);
+      struct statvfs disk;
+      statvfs(".", &disk);
+      printf("Total Disk Space: %ld GB", (disk.f_blocks * disk.f_frsize) / 1024 / 1024 / 1024);
+      printf("Available Disk Space: %ld GB", (disk.f_bavail * disk.f_frsize) / 1024 / 1024 / 1024);
 
       // Закрытие файла
       close(fd); 
