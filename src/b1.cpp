0709f51b85ff9eaea5a990c70f893164418a1666
SergeyBorBor <153349346+SergeyBorBor@users.noreply.github.com>
2024-01-04 17:50:49 +0300

    Update b1.cpp

--- src/b1.cpp
+++ src/b1.cpp
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctime>
+#include <fcntl.h>
+#include <sys/sysinfo.h>
+#include <sys/statvfs.h>
+#include <sys/resource.h>
+
 #define ROWS 10
 #define COLS 10
 
-int graph[ROWS][COLS];
+int graph[ROWS][COLS] = { // задаем матрицу планарного графа
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+        {1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+        {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+        {1, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+        {1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+        {1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+        {1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
 
 void generateGraph() {
     // Заполнение графа случайными значениями
@@ -85,21 +105,76 @@ int leeAlgorithm(int startX, int startY, int endX, int endY) {
 }
 
 int main() {
-    generateGraph();
-    printGraph();
+    pid_t pid;
+    pid = fork();
+    if(pid == 0)
+    {
+      printGraph();
 
-    int startX, startY, endX, endY;
-    printf("Введите координаты начальной вершины (x y): ");
-    scanf("%d %d", &startX, &startY);
-    printf("Введите координаты конечной вершины (x y): ");
-    scanf("%d %d", &endX, &endY);
+      int startX, startY, endX, endY;
+      printf("Введите координаты начальной вершины (x y): ");
+      scanf("%d %d", &startX, &startY);
+      printf("Введите координаты конечной вершины (x y): ");
+      scanf("%d %d", &endX, &endY);
 
-    int distance = leeAlgorithm(startX, startY, endX, endY);
-    if (distance == -1) {
+      int distance = leeAlgorithm(startX, startY, endX, endY);
+      if (distance == -1) 
+      {
         printf("Путь не найден\n");
-    } else {
+      } 
+      else 
+      {
         printf("Кратчайший путь между вершинами: %d\n", distance);
+      }
     }
+    else
+    {
+       printf(" Результаты мониторинга системы успешно записаны в файл log.txt.\n");
+       // Открытие файла log.txt для записи
+       int fd = open("../files/log.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+       if (fd == -1) 
+       {
+          perror("open");
+          exit(1);
+       }
+
+       // Дублирование дескриптора файлового дескриптора stdout для записи в файл
+       if (dup2(fd, STDOUT_FILENO) == -1) 
+       {
+         perror("dup2");
+         exit(1);
+       }
+
+       // Отображение информации о системе
+       printf("System Monitoring");
+
+      // Код мониторинга системы
+      // Выведем информацию о текущем процессе и системном времени
+      int pid = getpid();
+      printf("Current process ID: %d", pid);
+      time_t current_time = time(NULL);
+      printf("Current time: %s", ctime(&current_time));
+
+      // Получение информации об использовании оперативной памяти
+      struct sysinfo mem_info;
+      sysinfo(&mem_info);
+      printf("Total RAM: %ld MB", mem_info.totalram / 1024 / 1024);
+      printf("Free RAM: %ld MB", mem_info.freeram / 1024 / 1024);
+
+      // Получение информации об использовании процессора
+      struct rusage cpu_info;
+      getrusage(RUSAGE_SELF, &cpu_info);
+      printf("CPU Usage: %ld.%06ld seconds", cpu_info.ru_utime.tv_sec, cpu_info.ru_utime.tv_usec);
+
+      // Получение информации о дисковом пространстве
+      struct statvfs disk_info;
+      statvfs(".", &disk_info);
+      printf("Total Disk Space: %ld GB", (disk_info.f_blocks * disk_info.f_frsize) / 1024 / 1024 / 1024);
+      printf("Available Disk Space: %ld GB", (disk_info.f_bavail * disk_info.f_frsize) / 1024 / 1024 / 1024);
+
+      // Закрытие файла
+      close(fd); 
+  }
 
     return 0;
 }
