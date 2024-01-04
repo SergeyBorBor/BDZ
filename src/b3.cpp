60865baa4799332002f9f042bcb7c2a98ad24aa8
SergeyBorBor <153349346+SergeyBorBor@users.noreply.github.com>
2024-01-04 16:34:02 +0300

    Update b3.cpp

--- src/b3.cpp
+++ src/b3.cpp
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctime>
+#include <fcntl.h>
+#include <sys/sysinfo.h>
+#include <sys/statvfs.h>
+#include <sys/resource.h>
+#include <string.h>
 
+int main()
+{
+    FILE *fd
+    printf(" Результаты мониторинга системы успешно записаны в файл log.txt.\n");
+    fd = fopen("../filles/log.txt","w");
+    fputs("System Monitoring", fd);
+    fclose(fd);
+    return 0;
+}
