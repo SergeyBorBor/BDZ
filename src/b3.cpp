1d633e3ec3bf5c188e7916dcd0e609c76eec5ee0
SergeyBorBor <153349346+SergeyBorBor@users.noreply.github.com>
2024-01-04 16:58:27 +0300

    Update b3.cpp

--- src/b3.cpp
+++ src/b3.cpp
@@ -16,6 +16,14 @@ int main()
     printf(" Результаты мониторинга системы успешно записаны в файл log.txt.\n");
     fd = fopen("../filles/log.txt","w");
     fputs("System Monitoring", fd);
+
+    // Код мониторинга системы
+    // Выведем информацию о текущем процессе и системном времени
+    int pid = getpid();
+    fputs("Current process ID: %d", pid);
+    time_t current_time = time(0);
+   // printf("Current time: %s", ctime(&current_time));
+    
     fclose(fd);
     return 0;
 }
