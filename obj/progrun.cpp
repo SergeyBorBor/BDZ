393e5c1c7f2e047bd08c4cb1937e82239101a3ad
SergeyBorBor <153349346+SergeyBorBor@users.noreply.github.com>
2024-01-08 16:54:31 +0300

    Update progrun.cpp

--- obj/progrun.cpp
+++ obj/progrun.cpp
@@ -1,6 +1,6 @@
 #include <cstdlib>
 int main() {
-    // Запуск программы prog2.cpp
+    // Запуск программы prog1.cpp
     system("g++ ../Src/prog1.cpp -o prog1 -lm -lpthread && ./prog1");
 
     return 0;
